a84676d6914c7ef424825f6c0ddb343669aad3fc
Oluwatise Ajayi <dr393462@gmail.com>
2026-02-13 00:05:06 +0100

    used ts aias

--- backend/src/modules/story/story.service.ts
+++ backend/src/modules/story/story.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-import { StoryNode, StoryChoice, PlayerStoryState } from '../../generated/client';
+import { StoryNode, StoryChoice, PlayerStoryState } from '@prisma-local/client';
 
 @Injectable()
 export class StoryService {
